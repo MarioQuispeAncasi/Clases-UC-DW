a6fd7422598ef43ded7077388c364b7ad63fb822
MarioQuispeA <20181529@aloe.ulima.edu.pe>
2023-02-15 20:25:38 -0500

    commit

--- Clase13-Variables-y-Operadores/js/script.js
+++ Clase13-Variables-y-Operadores/js/script.js
@@ -1,10 +1,24 @@
+/* Variables */
+    /*Construccion */
 var numero1;
 var numero2;
 
 numero1 = 12;
 numero2 =21;
 
+var miVariable;
+var miVariableCompleta;
+
+    /*Asignacion de valor --> Operador */
+miVariable = "Soy una Variable";
+miVariableCompleta = miVariable + " Completa";
+
 function Suma(){
     var suma = numero1 + numero2;
-    alert("La suma del numero 1 y del numero 2 es " + suma);
+    alert("La suma del numero 1 y del numero 2 es " + suma
+    + " La suma del numero 1 y del numero 2 es " + suma);
+}
+
+function Mostrar(){
+    document.getElementById("textoMostrar").innerHTML = miVariableCompleta;
 }
