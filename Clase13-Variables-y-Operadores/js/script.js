e1c49491a0a635507ebbb206aacbedfb28d66abf
MarioQuispeA <20181529@aloe.ulima.edu.pe>
2023-02-15 20:10:54 -0500

    funciones intro

--- Clase13-Variables-y-Operadores/js/script.js
+++ Clase13-Variables-y-Operadores/js/script.js
@@ -1,3 +1,10 @@
-function Saludar(){
-    alert("Hola");
+var numero1;
+var numero2;
+
+numero1 = 12;
+numero2 =21;
+
+function Suma(){
+    var suma = numero1 + numero2;
+    alert("La suma del numero 1 y del numero 2 es " + suma);
 }
