ac739b0e834de95bb77b3f05027ba6082af70dc1
MarioQuispeA <20181529@aloe.ulima.edu.pe>
2023-02-20 20:10:12 -0500

    commit if

--- Clase15-Sentencias/js/script.js
+++ Clase15-Sentencias/js/script.js
@@ -1,5 +1,26 @@
 var texto = "Hola";
 
+
+/*
 function Saludar(){
-    alert(texto);
+    console.log("Enviando texto...");
+}*/
+
+function Comparar(){
+    let numero1 = document.getElementById("txtNumero1").value; 
+    let numero2 = document.getElementById("txtNumero2").value; 
+    let mensaje = "";
+
+    if (numero1>numero2) {
+        mensaje = numero1 + " es mayor que " + numero2;
+    }
+
+    if (numero1<numero2) {
+        mensaje = numero2 + " es mayor que " + numero1;
+    }
+
+    if (numero1==numero2) {
+        mensaje = "Ambos numeros non iguales";
+    }
+    document.getElementById("lblResultado").innerHTML = mensaje;
 }
