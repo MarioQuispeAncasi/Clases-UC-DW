746891d27b5bd6e637af58a026751b3e65f128c0
MarioQuispeA <20181529@aloe.ulima.edu.pe>
2023-02-27 22:38:45 -0500

    commit

--- Clase18-Windows/js/script.js
+++ Clase18-Windows/js/script.js
@@ -1,3 +1,28 @@
 function RecargarPagina() {
     window.location.reload(); 
 }
+
+function RecargaPausada() {
+    setTimeout(RecargarPagina,2000);
+}
+
+function RedireccionarNosotros() {
+    window.location.href = "/Nosotros.html";
+}
+
+function MoverObjeto(){
+    let numRandom1 = Math.random()*1440;
+    let numRandom2 = Math.random()*700;
+    document.getElementById("objetoJ").style.left = numRandom1 + "px";
+    document.getElementById("objetoJ").style.top = numRandom2 + "px";
+    numRandom1 = Math.random()*1440;
+    numRandom2 = Math.random()*700;
+    document.getElementById("objetoK").style.left = numRandom1 + "px";
+    document.getElementById("objetoK").style.top = numRandom2 + "px";
+    numRandom1 = Math.random()*1440;
+    numRandom2 = Math.random()*700;
+    document.getElementById("objetoL").style.left = numRandom1 + "px";
+    document.getElementById("objetoL").style.top = numRandom2 + "px";
+}
+
+setInterval(MoverObjeto,500);
