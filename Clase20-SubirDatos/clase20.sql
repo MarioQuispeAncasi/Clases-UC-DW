7af52c70a3131d85405c69cc7a2356eb28e0d00a
MarioQuispeA <20181529@aloe.ulima.edu.pe>
2023-03-03 22:02:54 -0500

    commit

--- Clase20-SubirDatos/clase20.sql
+++ Clase20-SubirDatos/clase20.sql
@@ -1,1 +1,3 @@
 USE empresa;
+
+SELECT * FROM empleado A;
---
+++ Clase20-SubirDatos/procedimiento.sql
@@ -0,0 +1,47 @@
+-- procedimiento
+/*crear nombre*/
+USE empresa;
+
+-- DECLARE nombre VARCHAR(300);
+-- SET nombre = 
+/*SET @nombre = 'Mario';
+SET @apellido = 'Quispe';
+SET @documento = '73212891';
+SET @vigente = 1;*/
+-- SELECT @nombre;
+
+INSERT INTO empleado (cnombres, capellidos, cdocumento, lvigente)
+VALUES
+(@nombre, @apellido, @documento, @vigente);
+
+SELECT * FROM empleado;
+
+/*DELETE FROM empleado WHERE idempleado = 3;
+
+UPDATE empleado SET idempleado = 1
+where cnombres = 'Mario';*/
+
+SET @nombre = '';
+SET @apellido = '';
+SET @documento = '';
+SET @vigente = 1;
+SET @indicador =0;
+
+BEGIN
+	IF(@indicador == 0) THEN
+		SELECT 'EL VALOR DEL INDICADOR ES 0'
+	END IF
+END
+
+
+CALL `CONSULTA_IF`();
+delete from empleado WHERE idempleado = 7;
+
+CALL CrearEmpleado();
+
+select * from empleado;
+
+CALL IngresarPlanilla();
+
+
+DELETE from empleado where idempleado > 0;
