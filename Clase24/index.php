38b09a6064b4073fbba3fb92efb41d7611f84d92
MarioQuispeA <20181529@aloe.ulima.edu.pe>
2023-03-15 19:57:11 -0500

    php

---
+++ Clase24/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="text-center bg-dark text-light">Mi primer documento en php</h1>
+    <a href="index.php" class="btn btn-primary">Inicio</a>
+    <a href="nosotros.php" class="btn btn-secondary">Nosotros</a>
+    <a href="servicios.php" class="btn btn-info">Servicios</a>
+    <a href="contactenos.php" class="btn btn-danger">Contactenos</a>
+    <button class="btn btn-danger"> Click</button>
+
+<?php
+    echo "Hola Mundo";
+    echo "Hola Mundo";
+    echo "Hola Mundo";
+    echo "Hola Mundo";
+    echo "Hola Mundo";
+    echo "Hola Mundo";
+    echo "Hola Mundo";
+    echo "Hola Mundo";
+    echo "<button class='btn btn-danger'> Click</button>"
+?>
+    
+
+</body>
+</html>
